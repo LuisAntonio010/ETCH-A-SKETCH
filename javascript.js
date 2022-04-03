f6c09286a87a8c096e873e27dd0d1ea75e718b3e
luisantonio010 <luisantoniomb03@gmail.com>
2022-04-03 13:25:25 -0400

    added grid

---
+++ javascript.js
@@ -0,0 +1,13 @@
+const container = document.getElementById("container")
+
+
+for (let i = 0; i < 256; i++ ){
+    const content = document.createElement('div')
+
+        content.classList.add('content')
+        container.appendChild(content)
+}
+
+
+
+
