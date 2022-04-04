abf3fede47697e6ea8d4dac07e4a82833ed0641c
luisantonio010 <luisantoniomb03@gmail.com>
2022-04-04 17:00:25 -0400

    project advancements

--- javascript.js
+++ javascript.js
@@ -1,13 +1,45 @@
 const container = document.getElementById("container")
+const boxes = document.querySelectorAll('.content')
+
+const btnSize = document.getElementById('btnSize')
 
 
-for (let i = 0; i < 256; i++ ){
-    const content = document.createElement('div')
+createBoard(25,25)
 
+function createBoard(col,rows){
+
+    for (let i = 0; i < (col * rows); i++ ){
+        const content = document.createElement('div')
         content.classList.add('content')
-        container.appendChild(content)
+        container.appendChild(content)    
+        
+        container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+
+    }
+}
+
+function userInput(){
+    
+
 }
 
 
+
+
+container.addEventListener('mouseover', function(e){
+    e.target.style.background = 'blue'
+})
+
+
+
+
+
+/*
+boxes.addEventListener('click', function onClick(event){
+event.target.style.backgroundColor ='blue'
+})
+
+*/
 
 
